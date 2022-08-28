75b7a70e780fefd2551fe2f9ed5c99e9b0e583cc
daisuke0207 <daisuke02074869@gmail.com>
2022-08-28 17:23:31 +0900

    player control 途中

---
+++ Assets/Props/Scripts/BaseLifetimeScope.cs
@@ -0,0 +1,14 @@
+using Props.Player;
+using VContainer;
+using VContainer.Unity;
+
+namespace Props
+{
+    public class BaseLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
