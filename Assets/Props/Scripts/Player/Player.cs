ce2870c68edb504e3d722beee3d06d50f9a623d7
daisuke0207 <daisuke02074869@gmail.com>
2022-08-29 07:54:05 +0900

    不要なコメント削除

--- Assets/Props/Scripts/Player/Player.cs
+++ Assets/Props/Scripts/Player/Player.cs
@@ -2,9 +2,6 @@ namespace Props.Player
 {
     public class Player
     {
-        // TODO: Playerは0個以上のカメラを持つ
-        // TODO: InputSystem（スマホ、開発用にPCでも？ => モバイルファースト）
-        // TODO: PlayerCamera（Free, スティック、視点、見ている方向に対して縦移動もする）
-        // TODO: カメラの切替メニュー
+        
     }
 }
